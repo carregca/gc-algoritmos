c068c5511346660370a47eb550471747a4fffcea
carre <fgarciacarrera@alumno.huergo.edu.ar>
2026-05-20 16:42:11 -0300

    insertion

--- ordenamiento/bubble vs selection.py
+++ ordenamiento/bubble vs selection.py
@@ -42,10 +42,22 @@ def selection_sort(lista):
 
     return lista
 
+def insertion_sort(lista):
+    n = len(lista)
+    for i in range(1, n):
+        actual = lista[i]
+        j = i - 1
+        while j >= 0 and lista[j] > actual:
+            lista[j + 1] = lista[j]
+            j -= 1
+        lista[j + 1] = actual
+    return lista
+
 tamaños = list(range(100, 5000, 100))
 
 tiempos_bubble = []
 tiempos_selection = []
+tiempos_insertion = []
 
 repeticiones = 5
 
@@ -53,6 +65,7 @@ for n in tamaños:
 
     total_bubble = 0
     total_selection = 0
+    total_insertion = 0
 
     for _ in range(repeticiones):
 
@@ -74,13 +87,22 @@ for n in tamaños:
         fin = time.perf_counter()
         total_selection += (fin - inicio)
 
+        #Insertion sort
+        lista_insertion = lista_original.copy()
+        inicio = time.perf_counter()
+        insertion_sort(lista_insertion)
+        fin = time.perf_counter()
+        total_insertion += (fin - inicio)
+
     tiempos_bubble.append(total_bubble / repeticiones)
     tiempos_selection.append(total_selection / repeticiones)
+    tiempos_insertion.append(total_insertion / repeticiones)
 
 
 plt.figure(figsize=(10,5))
 plt.plot(tamaños, tiempos_bubble, label="Bubble Sort")
 plt.plot(tamaños, tiempos_selection, label="Selection Sort")
+plt.plot(tamaños, tiempos_insertion, label="Insertion Sort")
 
 plt.title("Comparación Bubble Sort vs Selection Sort")
 plt.xlabel("Tamaño de la lista")
